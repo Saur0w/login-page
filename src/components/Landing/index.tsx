0c685668c8cab4124dd9a2e3927af5e032072bd7
saur0w <sthapliyal085@gmail.com>
2025-12-27 22:53:14 +0530

    "Initial Commit"

---
+++ src/components/Landing/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import styles from "./style.module.scss";
+import { gsap } from "gsap";
+
+export default function Landing() {
+    return (
+        <main className={styles.landing}>
+
+        </main>
+    )
+}
